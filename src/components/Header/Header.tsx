dcfb24f846584f297f6f37021bafdca6dcc7573f
FireSpirit171 <rhodyrev7@gmail.com>
2024-11-14 14:22:08 +0300

    Add navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,6 +12,11 @@ const Header: FC = () => {
                     <h1 className="header__title-container__title"><strong>Meat'n'Fish</strong></h1>
                 </div>
             </div>
+            <div className="header__button-container">
+                <Link to="/dishes">  
+                    <button className="header__menu-button">Меню</button>
+                </Link>
+            </div>
         </header>
     );
 }
