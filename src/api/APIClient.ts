e3cfd19c1041c728dc4d228b41fdc71fa8712219
FireSpirit171 <rhodyrev7@gmail.com>
2024-11-15 23:13:31 +0300

    Frontend proxy

--- src/api/APIClient.ts
+++ src/api/APIClient.ts
@@ -3,7 +3,7 @@
 import Ajax from "./Ajax.ts";
 
 const APIClient = {
-    BASE_URL: `http://${window.location.hostname}:8000`,
+    BASE_URL: `http://${window.location.hostname}:3000/api`,
 
     async getDishes(postfix: string){
         let url = this.BASE_URL + "/dishes/";
@@ -14,7 +14,7 @@ const APIClient = {
     },
 
     async getDish(id: string) {
-        const url = this.BASE_URL + `/dishes/${id}`;
+        const url = this.BASE_URL + `/dishes/${id}/`;
         return Ajax.get(url);
     }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/"),
+      },
+  },
   }, 
   plugins: [react()],
 })
