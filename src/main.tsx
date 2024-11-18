58303fe5b9e76eed0bd9d53b2e01917fdd0e8bfd
FireSpirit171 <rhodyrev7@gmail.com>
2024-11-19 01:17:25 +0300

    Started tauri

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/store'
-import './index.sass'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import './index.sass';
 
 import DishPage from './pages/DishPage/DishPage';
 import MainPage from './pages/MenuPage/MainPage';
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage/HomePage';
+import { useEffect } from 'react';
+
+import { invoke } from "@tauri-apps/api";
 
 const router = createBrowserRouter([
   {
@@ -36,8 +39,24 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-  <Provider store={store}> 
-    <RouterProvider router={router}></RouterProvider>
-  </Provider>
-)
+function App() {
+  useEffect(() => {
+    invoke('create')
+      .then((response: string) => console.log(response))
+      .catch((error: any) => console.error('Error:', error));
+
+    return () => {
+      invoke('close')
+        .then((response: string) => console.log(response))
+        .catch((error: any) => console.error('Error:', error));
+    };
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<App />);
