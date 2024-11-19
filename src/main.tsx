367a66fb21043a097821b53d8c7dd8196d846aeb
FireSpirit171 <rhodyrev7@gmail.com>
2024-11-19 21:42:07 +0300

    Finished deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,52 +1,57 @@
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/store'
-import './index.sass'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import './index.sass';
 
 import DishPage from './pages/DishPage/DishPage';
 import MainPage from './pages/MenuPage/MainPage';
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage/HomePage';
 
-const router = createBrowserRouter([
-  {
-    path: '/dishes/',
-    element: (
-      <Layout>
-        <MainPage />
-      </Layout>
-    ),
-  },
-  {
-    path: '/dishes/:id',
-    element: (
-      <Layout>
-        <DishPage />
-      </Layout>
-    ),
-  },
-  {
-    path: '/',
-    element: (
-      <Layout>
-        <HomePage />
-      </Layout>
-    ),
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/dishes/',
+      element: (
+        <Layout>
+          <MainPage />
+        </Layout>
+      ),
+    },
+    {
+      path: '/dishes/:id',
+      element: (
+        <Layout>
+          <DishPage />
+        </Layout>
+      ),
+    },
+    {
+      path: '/',
+      element: (
+        <Layout>
+          <HomePage />
+        </Layout>
+      ),
+    },
+  ],
+  // {
+  //   basename: '/MeatFish',
+  // }
+);
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}> 
+  <Provider store={store}>
     <RouterProvider router={router}></RouterProvider>
   </Provider>
-)
+);
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-      navigator.serviceWorker
-          .register("/MeatFish/sw.js")
-          .then(() => console.log("service worker registered"))
-          .catch((err) => console.log("service worker not registered", err));
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/MeatFish/sw.js')
+      .then(() => console.log('service worker registered'))
+      .catch((err) => console.log('service worker not registered', err));
   });
 }
