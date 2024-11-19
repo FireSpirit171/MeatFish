75805214cd8628312ddeb9d2c09c43b1eaa0a8a3
FireSpirit171 <rhodyrev7@gmail.com>
2024-11-19 05:17:24 +0300

    Change localhost at ip

--- src/api/APIClient.ts
+++ src/api/APIClient.ts
@@ -3,7 +3,7 @@
 import Ajax from "./Ajax.ts";
 
 const APIClient = {
-    BASE_URL: `http://${window.location.hostname}:3000/api/`,
+    BASE_URL: `http://192.168.102.224:3000/api`,
 
     async getDishes(postfix: string){
         let url = this.BASE_URL + "/dishes/";
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 3000,
     proxy: {
       "/api": {
-          target: "http://localhost:8000",
+          target: "http://192.168.102.224:8000",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, "/"),
       },
