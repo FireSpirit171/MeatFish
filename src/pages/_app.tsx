c78930cd6e01eeb71bb7b8faa01daad1eb1794e9
FireSpirit171 <rhodyrev7@gmail.com>
2024-12-01 17:55:50 +0300

    Add redux to nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../styles/globals.sass';
 import Header from '@/components/Header';
+import { Provider } from 'react-redux';
+import store from '../store/store';
+import { AppProps } from 'next/app';
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <Provider store={store}>
             <Header></Header>
             <Component {...pageProps} />
-        </>
+        </Provider>
     );
 }
 
