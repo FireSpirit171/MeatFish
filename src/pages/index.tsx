22f6fd18d5ec2fb8cebe3415c6c0891b58ec9cbe
FireSpirit171 <rhodyrev7@gmail.com>
2024-12-09 01:58:41 +0300

    Add carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,30 @@
 import { FC } from 'react';
-import { useRouter } from 'next/router';
+import Slider from 'react-slick'; 
 
 const HomePage: FC = () => {
-  const router = useRouter();
-
-  const handleButtonClick = () => {
-    router.push('/dishes');
+  const settings = {
+    dots: true, 
+    infinite: true, 
+    speed: 500, 
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
     <div className="home-page">
-      <img src="/homepage.gif" className="background-gif" alt="Background" />
+      <Slider {...settings}>
+        <div>
+          <img src="/homepage.gif" className="background-image" alt="Background gif" />
+        </div>
+        <div>
+          <img src="/homepage2.jpeg" className="background-image" alt="Background image 2" />
+        </div>
+        <div>
+          <img src="/homepage3.jpg" className="background-image" alt="Background image 3" />
+        </div>
+      </Slider>
       <div className="home-page__content">
         <h1 className="home-page__content__title">Добро пожаловать в наш ресторан!</h1>
-        <div className="home-page__content__buttons">
-          <button
-            className="home-page__content__buttons__button"
-            onClick={handleButtonClick}
-          >
-            Меню
-          </button>
-        </div>
       </div>
     </div>
   );
